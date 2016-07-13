13917f59fe55ba1c677b1c93dcc197e9b3082adb
gm42 <g.mazzotta@hotelspecials.nl>
2016-07-13 13:04:28 +0200

    Adding some constants/structs

--- shim.go
+++ shim.go
@@ -1,1 +1,12 @@
 package logrus
+
+type Logger struct {
+	// The logging level the logger should log at. This is typically (and defaults
+	// to) `logrus.Info`, which allows Info(), Warn(), Error() and Fatal() to be
+	// logged. `logrus.Debug` is useful in
+	Level Level
+}
+
+type Entry struct {
+	Logger *Logger
+}
